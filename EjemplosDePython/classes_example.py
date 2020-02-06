b99d5741bc3c0bc353e6e6a5ec1e2f2f48604cd8
edervs <eedervs.97@gmail.com>
2020-02-05 20:28:56 -0600

    Adding practice, presentations and examples

---
+++ EjemplosDePython/classes_example.py
@@ -0,0 +1,38 @@
+""" Classes example using inheritance. """
+
+class Animal:
+    """ Represents an animal. """
+
+    def __init__(self, age):
+        """ Animal constructor """
+        self.age = age
+
+    def breathe(self):
+        """ Breathes """
+        print("Breathing")
+
+    def eat(self, meal):
+        """ Eats the meal that is sent. """
+        self.breathe()
+        print("Eating " + meal)
+        self.breathe()
+    
+class Person(Animal):
+    job_office = ""
+
+    def __init__(self, age, job_office="Home"):
+        """ Person constructor. """
+        self.job_office = job_office
+        Animal.__init__(self, age)
+
+    def work(self):
+        """ Works in a place """
+        self.breathe()
+        print("Working at " + self.job_office)
+        self.eat("Lunch")
+
+
+person1 = Person(10)
+person2 = Person(25, job_office="Pedregal")
+person1.work()
+person2.work()
