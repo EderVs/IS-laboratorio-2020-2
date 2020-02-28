70c85027f8f336d57085d775d9e6584f3b4a7908
Eduardo Eder Vázquez <eedervs.97@gmail.com>
2020-02-27 18:09:30 -0600

    Feature/2 music app (#33)
    * Adding music app
    
    * Add top songs view
    
    * Adding music to INSTALLED_APPS

---
+++ TuMusica.com/TuMusica/music/views.py
@@ -0,0 +1,27 @@
+"""Music Views."""
+# Django
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    """Music index.
+
+    Showing some artists, songs, albums and playlists.
+    TODO: Show artists.
+    TODO: Show songs.
+    TODO: Show albums.
+    TODO: Show playlists.
+    """
+    return HttpResponse("<h1>This is the music index!<!h1>")
+
+
+def top_songs(request):
+    """Top songs.
+
+    TODO: Show songs by its popularity.
+    """
+    return HttpResponse(
+        "<head><title>Canciones populares</title></head>"
+        "<body><h1>Estas son las canciones populares:</h1></body>"
+    )
