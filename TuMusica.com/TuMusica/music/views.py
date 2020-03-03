bbac6abc89045fd9a3cdbdaf311de1941f497442
edervs <eedervs.97@gmail.com>
2020-03-02 19:46:08 -0600

    Adding Class-based views

--- TuMusica.com/TuMusica/music/views.py
+++ TuMusica.com/TuMusica/music/views.py
@@ -1,8 +1,10 @@
 """Music Views."""
 # Django
 from django.shortcuts import render
+from django.views import View
 
 
+# Function-based views.
 def index(request):
     """Music index.
 
@@ -23,3 +25,34 @@ def top_songs(request):
     """
     template = "music/top_songs.html"
     return render(request, template)
+
+
+# Class-based views.
+class Index(View):
+    """Music index.
+
+    Showing some artists, songs, albums and playlists.
+    TODO: Show artists.
+    TODO: Show songs.
+    TODO: Show albums.
+    TODO: Show playlists.
+    """
+
+    template = "music/index.html"
+
+    def get(self, request):
+        """GET method."""
+        return render(request, self.template)
+
+
+class TopSongs(View):
+    """Top songs.
+
+    TODO: Show songs by its popularity.
+    """
+
+    template = "music/top_songs.html"
+
+    def get(self, request):
+        """GET method."""
+        return render(request, self.template)
