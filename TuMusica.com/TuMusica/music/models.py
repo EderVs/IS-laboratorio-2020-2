12e10925305f16d638ca24dfecd8a82389f297a3
edervs <eedervs.97@gmail.com>
2020-03-09 20:55:40 -0600

    Adding Artist and Song Models

--- TuMusica.com/TuMusica/music/models.py
+++ TuMusica.com/TuMusica/music/models.py
@@ -1,3 +1,46 @@
+"""Music Models"""
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    """Artist Model."""
+
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        """Get str representation."""
+        return self.name
+
+    def __repr__(self):
+        """Get str representation."""
+        return self.__str__()
+
+
+def song_directory_path(instance, filename):
+    """Get song directory path to save."""
+    return f"music/songs/{instance.id}_{instance.name}_{filename}"
+
+
+class Song(models.Model):
+    """Song Model."""
+
+    name = models.CharField(max_length=200)
+    song_file = models.FileField(null=True, upload_to=song_directory_path)
+
+    # Relations
+    artists = models.ManyToManyField("music.Artist", related_name="songs")
+
+    def __str__(self):
+        """Get str representation."""
+        artists_str = ""
+        artists = list(self.artists.all())
+        if len(artists) == 0:
+            return f"{self.name}"
+        artists_str = f"{artists[0].name}"
+        for artist in artists[1:]:
+            artists_str += f", {artist.name}"
+        return f"{self.name} by {artists_str}"
+
+    def __repr__(self):
+        """Get str representation."""
+        return self.__str__()
