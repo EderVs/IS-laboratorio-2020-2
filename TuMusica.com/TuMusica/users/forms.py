7fb582e4c9a21a68ad05f7ff2c7a18c9f723b458
edervs <eedervs.97@gmail.com>
2020-03-17 14:22:10 -0600

    Validate that an email does not exists in the DB

--- TuMusica.com/TuMusica/users/forms.py
+++ TuMusica.com/TuMusica/users/forms.py
@@ -1,6 +1,7 @@
 """Users forms."""
 # Django
 from django import forms
+from django.contrib.auth.models import User
 
 
 class SignUpForm(forms.Form):
@@ -10,3 +11,11 @@ class SignUpForm(forms.Form):
     last_name = forms.CharField(max_length=200)
     email = forms.EmailField(max_length=200)
     password = forms.CharField(widget=forms.PasswordInput())
+
+    def clean_email(self):
+        """Validate that the email doesn't exist in the database."""
+        data = self.cleaned_data["email"]
+        if User.objects.filter(email=data).count() > 0:
+            raise forms.ValidationError("This email already exists.")
+
+        return data
