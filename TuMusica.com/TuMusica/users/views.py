8c6afaa6e61b92c48cfff9a6bbdd282cc1a180ce
edervs <eedervs.97@gmail.com>
2020-03-24 16:38:35 -0600

    Moving css and creating Extended user in Signup

--- TuMusica.com/TuMusica/users/views.py
+++ TuMusica.com/TuMusica/users/views.py
@@ -6,6 +6,9 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
+# Models
+from .models import ExtendedUser
+
 # Forms
 from users.forms import SignUpForm, LoginForm
 
@@ -29,13 +32,14 @@ class SignUp(View):
             context = {"form": form}
             return render(request, self.template, context)
 
-        User.objects.create_user(
+        user = User.objects.create_user(
             username=form.cleaned_data["email"],
             email=form.cleaned_data["email"],
             password=form.cleaned_data["password"],
             first_name=form.cleaned_data["first_name"],
             last_name=form.cleaned_data["last_name"],
         )
+        ExtendedUser.objects.create(user=user)
         return HttpResponse("<h1>User Created!</h1>")
 
 
