1fc8f0d1a2171c933c36dbb5066a59532d23ddf3
edervs <eedervs.97@gmail.com>
2020-03-23 20:34:30 -0600

    Adding Extended user and adding songs.html

--- TuMusica.com/TuMusica/users/models.py
+++ TuMusica.com/TuMusica/users/models.py
@@ -1,3 +1,22 @@
+"""Users models."""
+# Django
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Models
+from music.models import Song
+
+
+class ExtendedUser(models.Model):
+    """Extended user to save fav songs and playlists."""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    fav_songs = models.ManyToManyField("music.Song", related_name="users_fav")
+
+    def __str__(self):
+        """Get string representation."""
+        return f"User({self.user.id} {self.user.first_name})"
+
+    def __repr__(self):
+        """Get representation."""
+        return self.__str__()
