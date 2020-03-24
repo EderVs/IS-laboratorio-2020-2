b20f459a8dea198c90c3e1a6a7093dda37a5b2b1
edervs <eedervs.97@gmail.com>
2020-03-24 17:18:25 -0600

    Changing to PostgreSQL

--- TuMusica.com/TuMusica/TuMusica/settings.py
+++ TuMusica.com/TuMusica/TuMusica/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = "TuMusica.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USERNAME"),
+        "PASSWORD": os.environ.get("DB_USER_PASSWORD"),
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
